6726cda08f972c5db90925e2b5e8546f41de6e1c
ostapsulyk <ostapsulyk@gmail.com>
2021-04-27 21:28:01 -0400

    added pseudo code and removed previous js code

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,42 +1,51 @@
 "use strict";
 
-let currentlyDisplayed = document.getElementById("screen");
-function add() {
-  let num1 = currentlyDisplayed.innerHTML;
-}
+let onDisplay = document.getElementById("screen");
+
+let currentValue = 0;
+let oldValue;
+let containsDot = false;
 
 let buttons = document.querySelectorAll("button");
 buttons.forEach((element) => {
   element.addEventListener("click", () => {
-    // checking if pressed number
+    // if input is number
     if (element.classList.contains("num")) {
-      console.log("you pressed number");
-      buttons[0].innerHTML = "c";
-
-      // if displayed is 0
-      if (currentlyDisplayed.innerHTML === "0") {
-        //display number to the screen
-        currentlyDisplayed.innerHTML = element.value;
-        // else append number to the screen
-      } else {
-        currentlyDisplayed.innerHTML += element.value;
-      }
-    } else if (element.value === "+") {
-    }
-    if (element.value === "clear") {
-      currentlyDisplayed.innerHTML = "0";
-      buttons[0].innerHTML = "AC";
+      // if input is . and currentValue === 0 and containsDot === false
+    } else if (element.classList.contains("operations")) {
+    } else {
     }
   });
 });
 
-// when + pressed
-// save currently displayed to num1
-// on nest press button
-// function to get number
-// function to get operation
-// function to get number
-// call function corresponding to operation
+// if input is operation
+// call reset function
+// get input for operation
+// if input is operation again update operation
+// else if input is number
+// get input for number
 
-// evaluate two numbers to one
-// function to get another numbers
+// if input is top-row
+// if input is AC button
+// call reset function
+// else if input is +/-
+// multiply currentValue by -1
+// update screen
+// else
+// divide currentValue by 100
+// update screen
+
+// TODO: create reset function
+// set oldValue to currentValue
+// set newValue to 0
+// set containsDot to false
+// update display
+
+// TODO: create function to get input for number
+// TODO: create function to get input for operation
+// TODO: create function to get perform calculation
+
+// TODO: create main function
+
+// if displayed 0
+// if containsDot
