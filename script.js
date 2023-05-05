1f1c12c73cdb1a63d21a8c0e82ccaf0c3f3b5f75
Ostap <ostapsulyk@gmail.com>
2023-05-04 20:04:25 -0400

    implemented smooth scrolling

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-const scrollBtn = document.getElementById("learn-more-btn");
-const about = document.getElementById("about");
+// buttons
+const aboutBtn = document.getElementById("learn-more-btn");
+const projectBtn = document.getElementById("projects-btn");
+const navAboutBtn = document.querySelector(".nav-bar ul li:nth-child(1) a");
 
-scrollBtn.addEventListener("click", () => {
-  about.scrollIntoView({ behavior: "smooth" });
+const logo = document.querySelector(".logo");
+
+logo.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+// sections
+const aboutSection = document.querySelector(".about");
+const projectsSection = document.querySelector(".projects-section");
+
+aboutBtn.addEventListener("click", () => {
+  aboutSection.scrollIntoView({ behavior: "smooth" });
+});
+
+projectBtn.addEventListener("click", () => {
+  projectsSection.scrollIntoView({ behavior: "smooth" });
+});
+
+navAboutBtn.addEventListener("click", (event) => {
+  event.preventDefault(); // prevent default link behavior
+  aboutSection.scrollIntoView({ behavior: "smooth" });
+  navBar.classList.remove("active"); // hide navigation bar
+  heroContainer.classList.remove("slide-down"); // move hero section back up
 });
 
 const hamburger = document.querySelector(".hamburger");
